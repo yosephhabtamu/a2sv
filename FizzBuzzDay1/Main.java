17f616fdaadc33c6fbed64b6da4fb79303cce8cc
yosephhabtamu <yosephhabtamu5@gmail.com>
2022-03-25 23:50:37 +0300

    first attempt

--- FizzBuzzDay1/Main.java
+++
@@ -1,9 +0,0 @@
-package FizzBuzzDay1;
-
-public class Main {
-    public static void main(String[] args) {
-        FizBuzz fizBuzz = new FizBuzz();
-
-        System.out.println(fizBuzz.solution(15));
-    }
-}
