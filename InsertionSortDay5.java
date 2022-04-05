2d6af7d849bff22a9843cbf86b44c573002667df
yosephhabtamu <yosephhabtamu5@gmail.com>
2022-04-05 18:07:17 +0300

    commiting final solution

---
+++ InsertionSortDay5.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'insertionSort1' function below.
+     *
+     * The function accepts following parameters:
+     * 1. INTEGER n
+     * 2. INTEGER_ARRAY arr
+     */
+
+    public static void insertionSort1(int n, List<Integer> arr) {
+        // Write your code here
+
+    }
+
+}
+
+public class InsertionSortDay5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        Result.insertionSort1(n, arr);
+
+        bufferedReader.close();
+    }
+}
